0308d4e864c41c8d4d7e82ffde981ddf9a6e3d09
delixfe <delixfe@one-one-two.com>
2012-11-23 16:56:11 +0100

    Worked on integration tests.

--- Tests/validation-tests.js
+++ Tests/validation-tests.js
@@ -1419,17 +1419,20 @@ test('errorDetails property is not defined if enableErrorDetails equals false', 
 
 //#region azk integration tests
 test('Nested live grouping works with errorDetails - observable, live', function () {
+    ko.validation.init( { enableErrorDetails: true }, true);
     var vm = { array: ko.observableArray() };
     var item = { one:  ko.observable().extend( { required: true } ) };
     
-    var errors = ko.validation.group(vm, { deep: true, observable: true, live: true, errorDetails: true });
+    var errors = ko.validation.group(vm, { deep: true, observable: true, errorDetails: true, live: true, addResultToVM: false });
 
     vm.array.push(item);
     
     equals(errors().length, 1, 'grouping adds newly items newly inserted into observableArrays to result');
+    ko.validation.reset();
 });
 
 test('Adding to an observable array does not cause the reavaluation for every computed of errors for every observable', function () {
+    ko.validation.init( { enableErrorDetails: true }, true);
     var vm = { array: ko.observableArray() };
     var item = { one:  ko.observable().extend( { required: true } ) };
     
@@ -1443,5 +1446,6 @@ test('Adding to an observable array does not cause the reavaluation for every co
 
     vm.array.push(item);
     equals(computedHitCount, 2, ' first on while creating the computed, second one for adding the item');
+    ko.validation.reset();
 });
 //#endregion
